e22185c0c8b6b5280d9b64d842507b1cc3ead37c
Maxim Ermoshyn <emaxan1997@gmail.com>
2017-09-10 01:08:57 +0300

    First version

---
+++ SPP/SPP_Laba3/SPP_Laba3/ConcurrentAccumulator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SPP_Laba3
+{
+	public class ConcurrentAccumulator : IDisposable
+	{
+		private readonly List<object> _buffer = new List<object>();
+		private readonly int _maxObjectsCount;
+		private readonly int _timeSpan;
+		private readonly FlushCallback _onFlush;
+		private bool _timerOn;
+		
+		public event EventHandler<int> OnObjectCountChange;
+		public delegate void FlushCallback(List<object> items);
+
+
+		public ConcurrentAccumulator(int maxObjectsCount, int timeSpan, FlushCallback callback)
+		{
+			_maxObjectsCount = maxObjectsCount;
+			_timeSpan = timeSpan;
+			var timer = new Thread(TimerWork);
+			_onFlush = callback;
+			_timerOn = true;
+			timer.Start();
+		}
+
+
+		private void TimerWork(object state)
+		{
+			while(_timerOn)
+			{
+				lock(_buffer)
+				{
+					_onFlush(_buffer);
+					_buffer.Clear();
+				}
+				OnObjectCountChange?.Invoke(this, _buffer.Count);
+				Thread.Sleep(_timeSpan);
+			}
+		}
+
+		public void Add(object item)
+		{
+			lock(_buffer)
+			{
+				_buffer.Add(item);
+				if(_buffer.Count == _maxObjectsCount)
+				{
+					_onFlush(_buffer);
+					_buffer.Clear();
+				}
+			}
+			OnObjectCountChange?.Invoke(this, _buffer.Count);
+			Thread.Sleep(100);
+		}
+
+		#region IDisposable pattern
+
+		private void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				_timerOn = false;
+			}
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		~ConcurrentAccumulator()
+		{
+			Dispose(false);
+		}
+
+		#endregion
+	}
+}
---
+++ SPP/SPP_Laba3/SPP_Laba3/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SPP_Laba3
+{
+	public partial class MainWindow
+	{
+		private readonly List<Thread> _threads = new List<Thread>();
+		private readonly Dictionary<Thread, bool> _threadOff = new Dictionary<Thread, bool>();
+		private readonly ConcurrentAccumulator _accumulator;
+
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			_accumulator = new ConcurrentAccumulator(1000, 6000, OnFlush);
+			_accumulator.OnObjectCountChange += UpdateObjectCount;
+			LThreads.Content = $"Thread count: {_threads.Count}";
+		}
+		
+
+		private void OnFlush(List<object> items)
+		{
+			Dispatcher.Invoke(
+				() =>
+				{
+					LbLog.Items.Add("Flushing buffer");
+				});
+		}
+
+		private void UpdateObjectCount(object sender, int e)
+		{
+			Dispatcher.Invoke(
+				() =>
+				{
+					LObjects.Content = $"Objects in buffer: {e}";
+				});
+		}
+
+		private void TaskFunction(object arg)
+		{
+			while(true)
+			{
+				if(_threadOff[(Thread)arg])
+				{
+					_accumulator.Add(new object());
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+
+		private void BAdd_Click(object sender, RoutedEventArgs e)
+		{
+			var added = false;
+			var thread = new Thread(TaskFunction);
+
+			try
+			{
+				_threads.Add(thread);
+				_threadOff.Add(thread, true);
+				thread.Start(thread);
+				added = true;
+			}
+			catch(Exception exception)
+			{
+				LbLog.Items.Add(exception.Message);
+				_threads.Remove(thread);
+				_threadOff.Remove(thread);
+			}
+
+			if(added)
+			{
+				LThreads.Content = $"Thread count: {_threads.Count}";
+			}
+		}
+
+		private void BRemove_Click(object sender, RoutedEventArgs e)
+		{
+			if(_threads.Count == 0)
+			{
+				return;
+			}
+
+			var thread = _threads.First();
+			_threadOff[thread] = false;
+			_threads.Remove(thread);
+			LThreads.Content = $"Thread count: {_threads.Count}";
+			//thread.Join();
+		}
+
+		private void OnWindowClosing(object sender, CancelEventArgs e)
+		{
+			foreach(var t in _threads)
+			{
+				_threadOff[t] = false;
+			}
+			_accumulator.Dispose();
+		}
+	}
+}
