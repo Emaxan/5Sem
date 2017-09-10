a29b3d56e309455d87bbf18dafb4fce8390de134
Maxim Ermoshyn <emaxan1997@gmail.com>
2017-09-10 17:01:06 +0300

    Final version

---
+++ SPP/SPP_Laba4/SPP_Laba4/ArrayHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SPP_Laba4
+{
+	public static unsafe class ArrayHelper
+	{
+		public static unsafe long Sum(List<int> array)
+		{
+			long sum = 0;
+			var i = 0;
+			try
+			{
+				while (true)
+				{
+					sum += array[i++];
+				}
+			}
+			catch(ArgumentOutOfRangeException ex)
+			{
+				throw new OverflowException($"Index Out of range. Sum is {sum}.", ex);
+			}
+		}
+	}
+}
--- SPP/SPP_Laba4/SPP_Laba4/MainWindow.xaml.cs
+++ SPP/SPP_Laba4/SPP_Laba4/MainWindow.xaml.cs
@@ -1,28 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SPP_Laba4
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow
 	{
+		private List<int> _array;
+
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+
+		private void TbArrayLength_OnKeyDown(object sender, KeyEventArgs e)
+		{
+			switch (e.Key)
+			{
+				case Key.End:
+				case Key.Home:
+				case Key.Left:
+				case Key.Right:
+				case Key.Delete:
+				case Key.D0:
+				case Key.D1:
+				case Key.D2:
+				case Key.D3:
+				case Key.D4:
+				case Key.D5:
+				case Key.D6:
+				case Key.D7:
+				case Key.D8:
+				case Key.D9:
+				case Key.NumPad0:
+				case Key.NumPad1:
+				case Key.NumPad2:
+				case Key.NumPad3:
+				case Key.NumPad4:
+				case Key.NumPad5:
+				case Key.NumPad6:
+				case Key.NumPad7:
+				case Key.NumPad8:
+				case Key.NumPad9:
+					break;
+				default:
+					e.Handled = true;
+					break;
+			}
+		}
+
+		private void BGenerate_OnClick(object sender, RoutedEventArgs e)
+		{
+			if(!int.TryParse(TbArrayLength.Text, out int length))
+			{
+				MessageBox.Show("Array length not a number", "Error");
+				return;
+			}
+
+			SpArray.Children.Clear();
+			var rand = new Random(DateTime.UtcNow.Millisecond);
+			_array = new List<int>();
+			for(var i = 0; i < length; i++)
+			{
+				var number = (byte)(rand.Next(10)*10);
+				_array.Add(number);
+				var label = new Label
+							{
+								Content = number
+							};
+				SpArray.Children.Add(label);
+			}
+		}
+
+		private void BGetSum_Click(object sender, RoutedEventArgs e)
+		{
+			if(_array == null)
+			{
+				return;
+			}
+
+			try
+			{
+				ArrayHelper.Sum(_array);
+			}
+			catch (OverflowException ex)
+			{
+				MessageBox.Show(ex.Message, "Error");
+			}
 		}
 	}
 }
