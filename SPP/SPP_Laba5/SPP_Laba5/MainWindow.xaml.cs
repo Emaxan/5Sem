1d7fc0c6056da9c3dc592af9aae0e74e1cf24773
Maxim Ermoshyn <emaxan1997@gmail.com>
2017-09-10 21:10:26 +0300

    Update

--- SPP/SPP_Laba5/SPP_Laba5/MainWindow.xaml.cs
+++ SPP/SPP_Laba5/SPP_Laba5/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace SPP_Laba5
 {
@@ -20,9 +12,72 @@ namespace SPP_Laba5
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private Brush _color;
+
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+
+		private void CopyClick(object sender, MouseButtonEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void StopClick(object sender, MouseButtonEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Border_MouseEnter(object sender, MouseEventArgs e)
+		{
+			var border = sender as Border;
+			if(border == null)
+			{
+				return;
+			}
+
+			_color = border.BorderBrush;
+			border.BorderBrush = new SolidColorBrush(Colors.Black);
+		}
+
+		private void Border_MouseLeave(object sender, MouseEventArgs e)
+		{
+			var border = sender as Border;
+			if (border == null)
+			{
+				return;
+			}
+
+			border.BorderBrush = _color;
+		}
+
+		private void OpenSource_Click(object sender, RoutedEventArgs e)
+		{
+			var ofd = new OpenFileDialog
+					{
+						CheckFileExists = true,
+						CheckPathExists = true,
+						Multiselect = false,
+					};
+			if(ofd.ShowDialog() != true)
+			{
+				return;
+			}
+		}
+
+		private void OpenDestination_Click(object sender, RoutedEventArgs e)
+		{
+			var ofd = new OpenFileDialog
+					{
+						Multiselect = false,
+					};
+			if (ofd.ShowDialog() != true)
+			{
+				return;
+			}
 		}
 	}
 }
